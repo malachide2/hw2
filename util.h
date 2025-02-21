4df7fa6839758f11ecb70592c84efde412928637
Malachi Dewitt <mjdewitt@usc.edu>
2025-02-20 18:00:02 -0800

    Changed Something

--- util.h
+++ util.h
@@ -24,10 +24,10 @@ template <typename T>
 std::set<T> setUnion(std::set<T>& s1, std::set<T>& s2)
 {
 	std::set<T> output;
-	for (auto element : s1) {
+	for (auto& element : s1) {
 		output.insert(element);
 	}
-	for (auto element : s2) {
+	for (auto& element : s2) {
 		output.insert(element);
 	}
 	return output;
