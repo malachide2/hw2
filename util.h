df68104d843f37f25c2f111344d20fbead6ea39d
Malachi Dewitt <mjdewitt@usc.edu>
2025-02-20 23:03:05 -0800

    Wisdom has been provided (Small bug fix)

--- util.h
+++ util.h
@@ -24,10 +24,10 @@ template <typename T>
 std::set<T> setUnion(std::set<T>& s1, std::set<T>& s2)
 {
 	std::set<T> output;
-	for (T& element : s1) {
+	for (T element : s1) {
 		output.insert(element);
 	}
-	for (T& element : s2) {
+	for (T element : s2) {
 		output.insert(element);
 	}
 	return output;
